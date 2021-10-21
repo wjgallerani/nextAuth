9878615c50fdbff08406ca22dabef3e72eeff4f3
William Jose Gallerani <wjgallerani@saomanoel.com.br>
2021-10-20 23:24:18 -0300

    Atualização

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,14 @@
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupApiClient } from "../service/api";
 import { api } from "../service/apiClient";
 
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-    const { user } = useContext(AuthContext);
+    const { user, signOut } = useContext(AuthContext);
 
     useEffect(() => {
         api.get('/me')
@@ -15,7 +17,16 @@ export default function Dashboard() {
     }, []);
 
     return (
-        <h1>E-mail: {user?.email}</h1>
+        <>
+            <h1>E-mail: {user?.email}</h1>
+
+            <button onClick={signOut}>Sair</button>
+
+            <Can permissions={['metrics.list']}>
+                <div>Métricas</div>
+            </Can>
+
+        </>
     );
 }
 
---
+++ pages/metrics.tsx
@@ -0,0 +1,23 @@
+import { setupApiClient } from "../service/api";
+import { withSSRAuth } from "../utils/withSSRAuth";
+
+export default function Metrics() {
+
+    return (
+        <>
+            <h1>Metrics</h1>
+        </>
+    );
+}
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+    const apiClient = setupApiClient(ctx);
+    const response = await apiClient.get('/me');
+
+    return {
+        props: {}
+    }
+}, {
+    permissions: ['metrics.list3'],
+    roles: ['administrator']
+});
